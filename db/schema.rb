0c12e42177ed0f10e73410082f1eea1786aecc3a
Marcio Santos <mvs_designer@hotmail.com>
2018-09-13 08:29:56 -0300

    CRUD básico de emprestimo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180913105322) do
+ActiveRecord::Schema.define(version: 20180913112922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,13 @@ ActiveRecord::Schema.define(version: 20180913105322) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "loans", force: :cascade do |t|
+    t.date "loanDate"
+    t.date "returnDate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
