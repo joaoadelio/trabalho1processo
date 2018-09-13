64f352c8dfd5901971c44378af828697c87cc1df
Marcio Santos <mvs_designer@hotmail.com>
2018-09-13 15:16:51 -0300

    add columm active to books

---
+++ db/migrate/20180913181302_add_active_to_books.rb
@@ -0,0 +1,5 @@
+class AddActiveToBooks < ActiveRecord::Migration[5.1]
+  def change
+    add_column :books, :active, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180913133036) do
+ActiveRecord::Schema.define(version: 20180913181302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20180913133036) do
     t.datetime "updated_at", null: false
     t.integer "language_id"
     t.integer "bookcase_id"
+    t.boolean "active"
   end
 
   create_table "books_disciplines", id: false, force: :cascade do |t|
